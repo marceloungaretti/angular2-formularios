8955732ae5781078575031f7322493f61a733d3a
Marcelo <marcelo.ungaretti@gmail.com>
2022-01-25 15:29:29 -0300

    fim do capitulo 2

--- gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
+++ gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,4 +1,9 @@
+import { switchMap } from 'rxjs/operators';
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
 import { Component, OnInit } from '@angular/core';
+import { Animais } from '../animais';
+import { AnimaisService } from '../animais.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lista-animais',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  constructor() { }
+  animais$ !: Observable<Animais>;
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+  ) { }
 
   ngOnInit(): void {
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      switchMap((usuario) => {
+        const userName = usuario.name ?? '';
+        return this.animaisService.listaDoUsuario(userName);
+      })
+    )
+    // this.usuarioService.retornaUsuario().subscribe((usuario)=> {
+    //   const userName = usuario.name ?? '';
+    //   this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+    //     this.animais = animais;
+    //   })
+    // })
   }
 
 }
